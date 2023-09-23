903762521afdc268d0cc1f31b424447772b43458
Alexander <thesancheloo@gmail.com>
2023-09-23 23:43:50 +0300

    ui core files moved to ui/core

---
+++ app/src/main/kotlin/com/alex/myweather/core/ui_utils/theme/Color.kt
@@ -0,0 +1,82 @@
+package com.alex.myweather.core.ui_utils.theme
+
+import androidx.compose.ui.geometry.Offset
+import androidx.compose.ui.graphics.Brush
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.TileMode
+
+//Color scheme created via material3 theme builder
+
+//dark color scheme
+val PrimaryDark = Color(0xFFCBBEFF)
+val OnPrimaryDark = Color(0xFF322074)
+val PrimaryContainerDark = Color(0xFF49398C)
+val OnPrimaryContainerDark = Color(0xFFE6DEFF)
+
+val SecondaryDark = Color(0xFFFFACE9)
+val OnSecondaryDark = Color(0xFF57124D)
+val SecondaryContainerDark = Color(0xFF722B66)
+val OnSecondaryContainerDark = Color(0xFFFFD7F1)
+
+val TertiaryDark = Color(0xFFFFB2B7)
+val OnTertiaryDark = Color(0xFF64081E)
+val TertiaryContainerDark = Color(0xFF832232)
+val OnTertiaryContainerDark = Color(0xFFFFDADB)
+
+val SurfaceDark = Color(0xFF1C1B1E)
+val OnSurfaceDark = Color(0xFFE6E1E6)
+val SurfaceVariantDark = Color(0xFF48454E)
+val OnSurfaceVariantDark = Color(0xFFC9C4D0)
+
+val BackgroundDark = Color(0xFF1C1B1E)
+val OnBackgroundDark = Color(0xFFE6E1E6)
+
+val OutlineDark = Color(0xFF938F99)
+
+val ErrorDark = Color(0xFFFFB4AB)
+val OnErrorDark = Color(0xFF690005)
+
+
+//light color scheme
+val Primary = Color(0xFF6151A6)
+val OnPrimary = Color(0xFFFFFFFF)
+val PrimaryContainer = Color(0xFFE6DEFF)
+val OnPrimaryContainer = Color(0xFF1D0160)
+
+val Secondary = Color(0xFF370031)
+val OnSecondary = Color(0xFFFFFFFF)
+val SecondaryContainer = Color(0xFFDDF7F1)
+val OnSecondaryContainer = Color(0xFF390033)
+
+val Tertiary = Color(0xFF832232)
+val OnTertiary = Color(0xFFFFFFFF)
+val TertiaryContainer = Color(0xFFDDDADB)
+val OnTertiaryContainer = Color(0xFF410002)
+
+val Surface = Color(0xFFFFFBFF)
+val OnSurface = Color(0xFF1C1B1E)
+val SurfaceVariant = Color(0xFFE6E0EC)
+val OnSurfaceVariant = Color(0xFF48454E)
+
+val Background = Color(0xFFFFFBFF)
+val OnBackground = Color(0xFF1C1B1E)
+
+val Outline = Color(0xFF79757F)
+
+val Error = Color(0xFFBA1A1A)
+val OnError = Color(0xFFFFFFFF)
+
+// other colors
+val PersianOrange = Color(0xFFCE8964)
+val Mindaro = Color(0xFFEAF07C)
+val DodgerBlue = Color(0xFF2191FB)
+val OxfordBlue = Color(0xFF0A2239)
+
+val backgroundGradientLight = Brush.linearGradient(
+    colors = listOf(DodgerBlue, OxfordBlue),
+    start = Offset.Zero,
+    end = Offset.Infinite,
+    tileMode = TileMode.Clamp,
+)
+
+val backgroundGradientDark = Brush.linearGradient(listOf(Primary, Secondary, Tertiary))
---
+++ app/src/main/kotlin/com/alex/myweather/core/ui_utils/theme/Constants.kt
@@ -0,0 +1,13 @@
+package com.alex.myweather.core.ui_utils.theme
+
+const val LARGE_PADDING = 24
+const val MEDIUM_PADDING = 16
+const val SMALL_PADDING = 8
+
+const val LARGE_CORNER_RADIUS = 24
+const val MEDIUM_CORNER_RADIUS = 16
+const val SMALL_CORNER_RADIUS = 8
+
+const val WEATHER_INFO_LARGE_ICON_SIZE = 48
+const val WEATHER_INFO_MEDIUM_ICON_SIZE = 24
+const val WEATHER_INFO_SMALL_ICON_SIZE = 16
